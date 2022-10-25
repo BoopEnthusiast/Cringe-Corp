1336afa4a87953462097797f99fcf87f55392344
Boop <onpotis@gmail.com>
2022-10-25 21:30:46 +1300

    Sliiiide
    Sliiiiide made during code club

--- Godot project/Player.gd
+++ Godot project/Player.gd
@@ -1,11 +1,14 @@
 extends KinematicBody
 
 var speed = 7
+var slide_speed = 50
 const ACCEL_DEFAULT = 7
 const ACCEL_AIR = 1
+const ACCEL_SLIDE = 1000
 onready var accel = ACCEL_DEFAULT
 var gravity = 9.8
 var jump = 5
+var sliding = false
 
 var cam_accel = 40
 var mouse_sense = 0.1
@@ -43,11 +46,11 @@ func _process(delta):
 		
 func _physics_process(delta):
 	#get keyboard input
-	direction = Vector3.ZERO
 	var h_rot = global_transform.basis.get_euler().y
 	var f_input = Input.get_action_strength("move_backward") - Input.get_action_strength("move_forward")
-	var h_input = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	direction = Vector3(h_input, 0, f_input).rotated(Vector3.UP, h_rot).normalized()
+	var h_input = 0
+	if not sliding:
+		h_input = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	
 	#jumping and gravity
 	if is_on_floor():
@@ -62,9 +65,25 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		snap = Vector3.ZERO
 		gravity_vec = Vector3.UP * jump
+	if Input.is_action_just_released("slide"):
+		sliding = false
+	if Input.is_action_pressed("slide") and is_on_floor(): 
+		snap = Vector3.ZERO
+		if sliding == false:
+			slide_speed = 50
+		sliding = true
+		slide_speed -= 1
+		
+		
 	
 	#make it move
+	direction = Vector3.ZERO
+	direction = Vector3(h_input, 0, f_input).rotated(Vector3.UP, h_rot).normalized()
+	if sliding:
+		direction = Vector3(h_input, 0, -1).rotated(Vector3.UP, h_rot).normalized()
 	velocity = velocity.linear_interpolate(direction * speed, accel * delta)
+	if sliding:
+		velocity = velocity.linear_interpolate(direction * slide_speed, accel * delta)
 	movement = velocity + gravity_vec
-	
+	print(slide_speed)
 	move_and_slide_with_snap(movement, snap, Vector3.UP)
